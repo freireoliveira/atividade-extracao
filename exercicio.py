3ea566bf632d65ae572e7d9e8321c6ee77f41042
freireoliveira <85126641+freireoliveira@users.noreply.github.com>
2023-02-27 20:48:32 -0300

    Update exercicio.py

--- exercicio.py
+++ exercicio.py
@@ -1,26 +1,29 @@
-#%%
-import numpy as np
+import pandas as pd
+import requests as re
+from datetime import datetime as dt
 
-def chars(string):
-    return np.array(list(string))
+day_dict = {
+    1 : 'Monday',
+    2 : 'Tuesday',
+    3 : 'Wednesday',
+    4 : 'Thursday',
+    5 : 'Friday',
+    6 : 'Saturday',
+    7 : 'Sunday'
+}
 
-print(chars('oi tudo bem?'))
+table = pd.DataFrame()
 
-#%%
-def cripto(string):
-    original = np.array([ord(c) for c in string])
-    encripted = original * 33/2
-    return encripted
-
-def decrypt(encripted):
-    decrypted = encripted * 2/33
-    original = ''.join([chr(int(c)) for c in decrypted])
-    return original
-
-teste = cripto('oi tudo bem?')
-dep = decrypt(teste)
-print(teste, dep)
-
-#%%
-import matplotlib.pyplot as plt
-
+for year in range(2019, 2031):
+  request = re.get(f'https://calendarific.com/api/v2/holidays/?&api_key=5513a9d46231145743590d899efdcae3df210c60&country=BR&year={year}')
+  temp_table = pd.DataFrame(request.json()['response']['holidays'])
+  colunas = ['name', 'date']
+  temp_table = temp_table[~temp_table['name'].str.contains('Equinox', na = False)]
+  temp_table = temp_table[~temp_table['name'].str.contains('Solstice', na = False)].reset_index(drop=True)
+  temp_table = temp_table[colunas]
+  temp_table['date'] = [dt.strptime(x['iso'], '%Y-%m-%d') for x in temp_table['date']]
+  temp_table['weekday'] = [day_dict[temp_table['date'][i].isoweekday()] for i in range(len(temp_table))]
+  temp_table.columns = ['Name', 'Date', 'Weekday']
+  table = pd.concat([table,temp_table])
+  
+table = table.reset_index(drop=True)
